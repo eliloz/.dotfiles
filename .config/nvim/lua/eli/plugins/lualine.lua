df86bf927eae9d82c5ed1fad30b19f5b9a7e3313
Elias Lozano <eliloz@me.com>
2024-01-08 19:13:21 -0600

    Add .stylua.toml

--- .config/nvim/lua/eli/plugins/lualine.lua
+++ .config/nvim/lua/eli/plugins/lualine.lua
@@ -1,133 +1,133 @@
 local function getLspName()
-	local buf_clients = vim.lsp.buf_get_clients()
-	local buf_ft = vim.bo.filetype
-	if next(buf_clients) == nil then
-		return "  No servers"
-	end
-	local buf_client_names = {}
+  local buf_clients = vim.lsp.buf_get_clients()
+  local buf_ft = vim.bo.filetype
+  if next(buf_clients) == nil then
+    return "  No servers"
+  end
+  local buf_client_names = {}
 
-	for _, client in pairs(buf_clients) do
-		if client.name ~= "null-ls" then
-			table.insert(buf_client_names, client.name)
-		end
-	end
+  for _, client in pairs(buf_clients) do
+    if client.name ~= "null-ls" then
+      table.insert(buf_client_names, client.name)
+    end
+  end
 
-	local lint_s, lint = pcall(require, "lint")
-	if lint_s then
-		for ft_k, ft_v in pairs(lint.linters_by_ft) do
-			if type(ft_v) == "table" then
-				for _, linter in ipairs(ft_v) do
-					if buf_ft == ft_k then
-						table.insert(buf_client_names, linter)
-					end
-				end
-			elseif type(ft_v) == "string" then
-				if buf_ft == ft_k then
-					table.insert(buf_client_names, ft_v)
-				end
-			end
-		end
-	end
+  local lint_s, lint = pcall(require, "lint")
+  if lint_s then
+    for ft_k, ft_v in pairs(lint.linters_by_ft) do
+      if type(ft_v) == "table" then
+        for _, linter in ipairs(ft_v) do
+          if buf_ft == ft_k then
+            table.insert(buf_client_names, linter)
+          end
+        end
+      elseif type(ft_v) == "string" then
+        if buf_ft == ft_k then
+          table.insert(buf_client_names, ft_v)
+        end
+      end
+    end
+  end
 
-	local ok, conform = pcall(require, "conform")
-	local formatters = table.concat(conform.formatters_by_ft[vim.bo.filetype], " ")
-	if ok then
-		for formatter in formatters:gmatch("[%w][_%w]*") do
-			if formatter then
-				table.insert(buf_client_names, formatter)
-			end
-		end
-	end
+  local ok, conform = pcall(require, "conform")
+  local formatters = table.concat(conform.formatters_by_ft[vim.bo.filetype], " ")
+  if ok then
+    for formatter in formatters:gmatch("[%w][_%w]*") do
+      if formatter then
+        table.insert(buf_client_names, formatter)
+      end
+    end
+  end
 
-	local hash = {}
-	local unique_client_names = {}
+  local hash = {}
+  local unique_client_names = {}
 
-	for _, v in ipairs(buf_client_names) do
-		if not hash[v] then
-			unique_client_names[#unique_client_names + 1] = v
-			hash[v] = true
-		end
-	end
-	local language_servers = table.concat(unique_client_names, ", ")
+  for _, v in ipairs(buf_client_names) do
+    if not hash[v] then
+      unique_client_names[#unique_client_names + 1] = v
+      hash[v] = true
+    end
+  end
+  local language_servers = table.concat(unique_client_names, ", ")
 
-	return " " .. language_servers
+  return " " .. language_servers
 end
 
 local lsp = {
-	function()
-		return getLspName()
-	end,
+  function()
+    return getLspName()
+  end,
 }
 
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		local lualine = require("lualine")
-		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local lualine = require("lualine")
+    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
-		local colors = {
-			blue = "#65D1FF",
-			green = "#3EFFDC",
-			violet = "#FF61EF",
-			yellow = "#FFDA7B",
-			red = "#FF4A4A",
-			fg = "#c3ccdc",
-			bg = "#112638",
-			inactive_bg = "#2c3043",
-		}
+    local colors = {
+      blue = "#65D1FF",
+      green = "#3EFFDC",
+      violet = "#FF61EF",
+      yellow = "#FFDA7B",
+      red = "#FF4A4A",
+      fg = "#c3ccdc",
+      bg = "#112638",
+      inactive_bg = "#2c3043",
+    }
 
-		local my_lualine_theme = {
-			normal = {
-				a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			insert = {
-				a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			visual = {
-				a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			command = {
-				a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			replace = {
-				a = { bg = colors.red, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			inactive = {
-				a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
-				b = { bg = colors.inactive_bg, fg = colors.semilightgray },
-				c = { bg = colors.inactive_bg, fg = colors.semilightgray },
-			},
-		}
+    local my_lualine_theme = {
+      normal = {
+        a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
+        b = { bg = colors.bg, fg = colors.fg },
+        c = { bg = colors.bg, fg = colors.fg },
+      },
+      insert = {
+        a = { bg = colors.green, fg = colors.bg, gui = "bold" },
+        b = { bg = colors.bg, fg = colors.fg },
+        c = { bg = colors.bg, fg = colors.fg },
+      },
+      visual = {
+        a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
+        b = { bg = colors.bg, fg = colors.fg },
+        c = { bg = colors.bg, fg = colors.fg },
+      },
+      command = {
+        a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
+        b = { bg = colors.bg, fg = colors.fg },
+        c = { bg = colors.bg, fg = colors.fg },
+      },
+      replace = {
+        a = { bg = colors.red, fg = colors.bg, gui = "bold" },
+        b = { bg = colors.bg, fg = colors.fg },
+        c = { bg = colors.bg, fg = colors.fg },
+      },
+      inactive = {
+        a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
+        b = { bg = colors.inactive_bg, fg = colors.semilightgray },
+        c = { bg = colors.inactive_bg, fg = colors.semilightgray },
+      },
+    }
 
-		-- configure lualine with modified theme
-		lualine.setup({
-			options = {
-				theme = my_lualine_theme,
-			},
-			sections = {
-				lualine_x = {
-					lsp,
-					{
-						lazy_status.updates,
-						cond = lazy_status.has_updates,
-						color = { fg = "#ff9e64" },
-					},
-					{ "encoding" },
-					{ "fileformat" },
-					{ "filetype" },
-				},
-			},
-		})
-	end,
+    -- configure lualine with modified theme
+    lualine.setup({
+      options = {
+        theme = my_lualine_theme,
+      },
+      sections = {
+        lualine_x = {
+          lsp,
+          {
+            lazy_status.updates,
+            cond = lazy_status.has_updates,
+            color = { fg = "#ff9e64" },
+          },
+          { "encoding" },
+          { "fileformat" },
+          { "filetype" },
+        },
+      },
+    })
+  end,
 }
