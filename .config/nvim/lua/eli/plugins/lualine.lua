77a55455e7f0c5676a3a4e00714b6ea15a87e133
Elias Lozano <eliloz@me.com>
2024-01-09 19:15:09 -0600

    Add separate lsp, format, and linting sections to lualine

--- .config/nvim/lua/eli/plugins/lualine.lua
+++ .config/nvim/lua/eli/plugins/lualine.lua
@@ -1,54 +1,74 @@
-local function getLspName()
-  local buf_clients = vim.lsp.buf_get_clients()
-  local buf_ft = vim.bo.filetype
-  if next(buf_clients) == nil then
-    return "  No servers"
+local function getFormatters()
+  -- Check if 'conform' is available
+  local status, conform = pcall(require, "conform")
+  if not status then
+    return "Conform not installed"
   end
-  local buf_client_names = {}
 
-  for _, client in pairs(buf_clients) do
-    if client.name ~= "null-ls" then
-      table.insert(buf_client_names, client.name)
+  local lsp_format = require("conform.lsp_format")
+
+  -- Get formatters for the current buffer
+  local formatters = conform.list_formatters_for_buffer()
+  if formatters and #formatters > 0 then
+    local formatterNames = {}
+
+    for _, formatter in ipairs(formatters) do
+      table.insert(formatterNames, formatter)
     end
+
+    return "󰷈 " .. table.concat(formatterNames, " ")
   end
 
+  -- Check if there's an LSP formatter
+  local bufnr = vim.api.nvim_get_current_buf()
+  local lsp_clients = lsp_format.get_format_clients({ bufnr = bufnr })
+
+  if not vim.tbl_isempty(lsp_clients) then
+    return "󰷈 LSP Formatter"
+  end
+
+  return ""
+end
+
+local function getLinters()
+  local buf_lint_names = {}
+  local buf_ft = vim.bo.filetype
   local lint_s, lint = pcall(require, "lint")
   if lint_s then
     for ft_k, ft_v in pairs(lint.linters_by_ft) do
       if type(ft_v) == "table" then
         for _, linter in ipairs(ft_v) do
           if buf_ft == ft_k then
-            table.insert(buf_client_names, linter)
+            table.insert(buf_lint_names, linter)
           end
         end
       elseif type(ft_v) == "string" then
         if buf_ft == ft_k then
-          table.insert(buf_client_names, ft_v)
+          table.insert(buf_lint_names, ft_v)
         end
       end
     end
   end
 
-  local ok, conform = pcall(require, "conform")
-  local formatters = table.concat(conform.formatters_by_ft[vim.bo.filetype], " ")
-  if ok then
-    for formatter in formatters:gmatch("[%w][_%w]*") do
-      if formatter then
-        table.insert(buf_client_names, formatter)
-      end
-    end
+  if not vim.tbl_isempty(buf_lint_names) then
+    return "󰱺 " .. table.concat(buf_lint_names, " ")
   end
 
-  local hash = {}
-  local unique_client_names = {}
+  return ""
+end
 
-  for _, v in ipairs(buf_client_names) do
-    if not hash[v] then
-      unique_client_names[#unique_client_names + 1] = v
-      hash[v] = true
-    end
+local function getLspName()
+  local buf_clients = vim.lsp.buf_get_clients()
+  if next(buf_clients) == nil then
+    return "  No servers"
   end
-  local language_servers = table.concat(unique_client_names, ", ")
+  local buf_client_names = {}
+
+  for _, client in pairs(buf_clients) do
+    table.insert(buf_client_names, client.name)
+  end
+
+  local language_servers = table.concat(buf_client_names, ", ")
 
   return " " .. language_servers
 end
@@ -56,6 +76,18 @@ end
 local lsp = {
   function()
     return getLspName()
+  end,
+}
+
+local fmt = {
+  function()
+    return getFormatters()
+  end,
+}
+
+local lnt = {
+  function()
+    return getLinters()
   end,
 }
 
@@ -118,6 +150,8 @@ return {
       sections = {
         lualine_x = {
           lsp,
+          lnt,
+          fmt,
           {
             lazy_status.updates,
             cond = lazy_status.has_updates,
